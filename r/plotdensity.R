6a8fa651e2e3cef0e697135df65accb1d2815254
uredkar <unmesh_redkar@hotmail.com>
2023-04-26 10:25:07 -0500

    moving diagram

---
+++ r/plotdensity.R
@@ -0,0 +1,11 @@
+#Create a sequence of 100 equally spaced numbers between -4 and 4
+x <- seq(-4, 4, length=100)
+
+#create a vector of values that shows the height of the probability distribution
+#for each value in x
+y <- dnorm(x,mean=0.10,sd=0.10)
+
+#plot x and y as a scatterplot with connected lines (type = "l") and add
+#an x-axis with custom labels
+plot(x,y, type = "l", lwd = 2, axes = FALSE, xlab = "", ylab = "")
+axis(1, at = -3:3, labels = c("-3s", "-2s", "-1s", "mean", "1s", "2s", "3s"))
